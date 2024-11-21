25e82fe3a70bdf6ec2a8bbde8312b52af0e291b3
PaigeN2026 <Paige.nunez2026@gbwl.org>
2024-11-21 13:54:54 +0000

    added for loops

--- Main.java
+++ Main.java
@@ -29,6 +29,34 @@ public class Main {
          count++; // equivalent to count = count + 1;
       }
 
+      // FOR LOOPS are "shortcuts" for looping when you know 
+      // how many times the loop should run
+      // all loop CONTROL info is in the header
+      for (int i = 0; i < 13; i++) {
+         // LOOP BODY (process to be repeated)
+         System.out.println(i + 1);
+      }
+
+         // You can change the variable by anything
+         // What does this loop do? -> counting even numbers
+         // How many times does it loop? ->
+         for (int i = 8; i <= 22; i+=2) {
+            System.out.println(i);
+      }
+
+      // BACKWARDS looping
+      // You must change all 3 loop control parts!
+      for (int i = 12; i >= 1; i--) {
+         System.out.println(i + " bottles of coke on the wall. ");
+         System.out.println(i + " bottles of coke. ");
+         System.out.println("Take one down, pass it around. ");
+         System.out.println((i - 1) +  " bottles of coke on the wall!");
+         // check if on the last bottle (final iteration)
+         if (i == 1) {
+            System.out.println("They're all gone.");
+         }
+      }
+     
       // GUESSING GAME
       Scanner scan = new Scanner(System.in);
       // Choose a random number from 0-100
@@ -57,5 +85,7 @@ public class Main {
 
       // AFER LOOP ENDS (meaning guess == num4)
       System.out.println("You got it!");
+
+
    }
 }
